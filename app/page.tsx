41521a1572dcf38a4f308ead05dd53c10c48df29
jan <janruebl@gmail.com>
2026-02-04 15:21:26 +0100

    bla

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col ">
-     <Link href="/examplepage" className="m-4 p-4 bg-blue-500 text-white rounded">
-        Go to Example Page
-      </Link>
-      <Link href="/testpage" className="m-4 p-4 bg-green-500 text-white rounded">
+    <div className='flex flex-col '>
+      <Button
+        variant={"link"}
+        className='m-4 p-4 bg-green-500 text-white rounded'>
+        <Link href='/examplepage'>Go to Example Page</Link>
+      </Button>
+      <Link
+        href='/testpage'
+        className='m-4 p-4 bg-green-500 text-white rounded'>
         Go to Test Page
       </Link>
-      </div>
+    </div>
   );
 }
--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -1,11 +1,14 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 const Testpage = () => {
   return (
     <div>
-      <Button size={"xs"}>Default shadcn Button</Button>
+      <Card>
+        <Button size={"xs"}>Default shadcn Button</Button>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Testpage
+export default Testpage;
